75b76d660bfd8699aa746414c93c5b1afbcc589a
pipas <ismailpipas@gmail.com>
2024-06-24 14:28:38 +0100

    feat: back side card new style

---
+++ src/components/organisms/card-back-side.tsx
@@ -0,0 +1,80 @@
+import { Code, ExternalLink, Plus } from "lucide-react";
+import React from "react";
+import { EvervaultCard } from "../ui/evervault-card";
+import { cn } from "@/lib/utils";
+import { Button } from "../atoms/button";
+
+const CardBackSide = ({
+  className,
+  title,
+  description,
+  url,
+  sourceCode,
+  tags,
+  label,
+}: {
+  className?: string;
+  title: string;
+  description: string;
+  url?: string;
+  sourceCode?: string;
+  label?: string;
+  tags?: string[];
+}) => {
+  return (
+    <div
+      className={cn(
+        "border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]",
+        className,
+      )}
+    >
+      <Plus className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Plus className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Plus className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Plus className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+
+      <div className="flex flex-col flex-grow w-full">
+        <EvervaultCard text={label} />
+
+        <h2 className="dark:text-white text-black mt-4 text-sm font-light">{description}</h2>
+        <div className="flex gap-x-2">
+          {tags?.map((tag) => (
+            <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
+              {tag}
+            </p>
+          ))}
+        </div>
+      </div>
+      <div className="flex gap-x-4 pt-8 justify-end w-full">
+        {url ? (
+          <Button
+            variant="primary"
+            size="regular"
+            radius="none"
+            className="min-w-24 gap-x-2"
+            onClick={() => window.open(url, "_blank")}
+          >
+            <ExternalLink size={18} /> Visit
+          </Button>
+        ) : (
+          ""
+        )}
+        {sourceCode ? (
+          <Button
+            variant="primary"
+            size="regular"
+            radius="none"
+            className="min-w-24 gap-x-2"
+            onClick={() => window.open(sourceCode, "_blank")}
+          >
+            <Code size={18} /> Source
+          </Button>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CardBackSide;
--- src/components/ui/glare-card.tsx
+++ src/components/ui/glare-card.tsx
@@ -28,7 +28,7 @@ export const GlareCard = ({ children, className }: { children: React.ReactNode; 
     "--duration": "300ms",
     "--foil-size": "100%",
     "--opacity": "0",
-    "--radius": "24px",
+    "--radius": "0px",
     "--easing": "ease",
     "--transition": "var(--duration) var(--easing)",
   } as any;
@@ -110,9 +110,9 @@ export const GlareCard = ({ children, className }: { children: React.ReactNode; 
         }
       }}
     >
-      <div className="h-full grid will-change-transform origin-center transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] [transform:rotateY(var(--r-x))_rotateX(var(--r-y))] rounded-3xl border-slate-800 hover:[--opacity:0.6] hover:[--duration:200ms] hover:[--easing:linear] hover:filter-none overflow-hidden shadow-2xl">
+      <div className="h-full grid will-change-transform origin-center transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] [transform:rotateY(var(--r-x))_rotateX(var(--r-y))] rounded-2xl border-slate-800 hover:[--opacity:0.6] hover:[--duration:200ms] hover:[--easing:linear] hover:filter-none overflow-hidden shadow-2xl">
         <div className="w-full h-full grid [grid-area:1/1] mix-blend-soft-light [clip-path:inset(0_0_0_0_round_var(--radius))]">
-          <div className={cn("h-full w-full bg-zinc-700", className)}>{children}</div>
+          <div className={cn("h-full w-full bg-slate-800", className)}>{children}</div>
         </div>
         <div className="w-full h-full grid [grid-area:1/1] mix-blend-soft-light [clip-path:inset(0_0_1px_0_round_var(--radius))] opacity-[var(--opacity)] transition-opacity transition-background duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] will-change-background [background:radial-gradient(farthest-corner_circle_at_var(--m-x)_var(--m-y),_rgba(255,255,255,0.8)_10%,_rgba(255,255,255,0.65)_20%,_rgba(255,255,255,0)_90%)]" />
         <div

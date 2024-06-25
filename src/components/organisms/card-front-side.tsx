3c4aa5f525917ba10d448b63331575f59a7e88d2
pipas <ismailpipas@gmail.com>
2024-06-25 12:41:46 +0100

    feat: new front card style

---
+++ src/components/organisms/card-front-side.tsx
@@ -0,0 +1,18 @@
+import { Plus } from "lucide-react";
+import React, { ReactNode } from "react";
+import { EvervaultCard } from "../ui/evervault-card";
+
+const CardFrontSide = ({ children }: { children: string }) => {
+  return (
+    <div className="relative w-full h-full top-0 left-0 backface-visibility-hidden bg-black border-white/20 border">
+      <Plus className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Plus className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Plus className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Plus className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+
+      <EvervaultCard text={children} className="h-full aspect-auto w-full" />
+    </div>
+  );
+};
+
+export default CardFrontSide;

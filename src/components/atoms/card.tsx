16e6ff23da7169c042b7a7e5c3ebb08b41d07bc6
pipas <ismailpipas@gmail.com>
2024-06-25 11:49:00 +0100

    feat: screenshot image instead of evervault card annimation

--- src/components/atoms/card.tsx
+++ src/components/atoms/card.tsx
@@ -51,21 +51,16 @@ export const Card = ({
         <GlareCard className="flex items-center justify-center w-full h-full top-0 left-0 overflow-hidden backface-visibility-hidden">
           <p className="text-foreground">{label}</p>
         </GlareCard>
-        {/* Back side */}
-        <div
-          className="absolute top-1/2 left-1/2 max-w-[calc(100vw-40px)] w-[350px] 2xl:w-[450px] min-h-[430px] h-[calc(100vh-400px)] md:h-[460px] 2xl:h-auto shadow-lg max-h-[700px] rounded-lg md:rounded-xl backface-visibility-hidden flex flex-wrap gap-y-4 content-between cursor-auto"
-          style={{ transform: "rotateY(180deg) translateX(50%) translateY(-50%)" }}
-        >
-          <CardBackSide
-            description={description}
-            sourceCode={sourceCode}
-            tags={tags}
-            title={title}
-            label={label}
-            url={url}
-            className="bg-white dark:bg-black w-full h-full"
-          />
-        </div>
+        <CardBackSide
+          description={description}
+          sourceCode={sourceCode}
+          tags={tags}
+          title={title}
+          label={label}
+          url={url}
+          screenshot={screenshot}
+          className="bg-white dark:bg-black w-full h-full"
+        />
       </div>
     </div>
   );

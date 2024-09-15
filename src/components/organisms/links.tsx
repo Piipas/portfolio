6b1e8c0a2b5e2c4519f97d3d2660466c1aaa897b
pipas <ismailpipas@gmail.com>
2024-09-15 20:10:58 +0100

    feat: added resume button

--- src/components/organisms/links.tsx
+++ src/components/organisms/links.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/atoms/button";
-import { cn } from "@/lib/utils";
 import { useCards } from "@/providers/cards-provider";
 import { motion, Variants } from "framer-motion";
 import { File, Github, Layers, Linkedin, Mail } from "lucide-react";
@@ -21,15 +20,7 @@ const links: Link[] = [
     point: true,
   },
   {
-    icon: (
-      // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-6">
-      //   <path
-      //     d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
-      //     fill="currentColor"
-      //   />
-      // </svg>
-      <Linkedin />
-    ),
+    icon: <Linkedin />,
     url: "https://linkedin.com/in/ismail-baalouk",
     label: "LinkedIn",
     point: false,
@@ -40,16 +31,16 @@ const links: Link[] = [
     label: "Github",
     point: false,
   },
-  // {
-  //   icon: <File />,
-  //   url: "mailto:is.baalouk@gmail.com",
-  //   label: "Resume",
-  //   point: false,
-  // },
   {
     icon: <Mail />,
     url: "mailto:is.baalouk@gmail.com",
     label: "Contact",
+    point: false,
+  },
+  {
+    icon: <File />,
+    url: "/resume.pdf",
+    label: "Resume",
     point: false,
   },
 ];
